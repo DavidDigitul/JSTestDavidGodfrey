66f8fe5e603e389572005ac5e7cd7743fa3cc67c
DavidDigitul <43763813+DavidDigitul@users.noreply.github.com>
2018-10-01 21:47:23 +0100

    Add files via upload
    JS and HTML files uploaded containing the completed functions and very basic front end functionality to see the functions working correctly.

---
+++ JS/Color.js
@@ -0,0 +1,114 @@
+﻿/*
+utility functions
+*/
+
+function elById(ID) {
+    return document.getElementById(ID);
+}
+
+function hexToRgb(hex) {
+    return /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+}
+
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length === 1 ? "0" + hex : hex;
+}
+
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+
+function validateHex(color) {
+
+    var isOk = /^[0-9A-F]{6}$/i.test(color);
+
+    return (isOk);
+}
+
+function getMean(val1, val2) {
+    var mean = 0;
+
+    mean = Math.round((val1 + val2) / 2);
+
+    return mean;
+}
+/*
+End utility functions
+*/
+
+
+
+/*
+Main Color functions
+*/
+function averageColor(color1, color2, rgb) {
+
+    //get rgb arrays
+    var color1Result = hexToRgb(color1);
+    var color2Result = hexToRgb(color2);
+
+    //get mean of r
+    var r = getMean(parseInt(color1Result[1], 16),
+        parseInt(color2Result[1], 16));
+    //get mean of g
+    var g = getMean(parseInt(color1Result[2], 16),
+        parseInt(color2Result[2], 16));
+    //get mean of b
+    var b = getMean(parseInt(color1Result[3], 16),
+        parseInt(color2Result[3], 16));
+
+    if (rgb) {
+        //return new color
+        return ["rgb(", r, ",", g, ",", b, ")"].join("");
+    }
+    else {
+        return rgbToHex(r, g, b);
+    }
+
+}
+/*
+End Main Color functions
+*/
+
+
+
+/*
+UI input for testing of colory function
+*/
+function submitColorFunction() {
+
+    //get color 1
+    var color1 = elById("color1").value;
+    //get color 2
+    var color2 = elById("color2").value;
+
+    //get feedback div's'
+    var infoDev = elById("colorInfo");
+    var feedbackDiv = elById("colorDisplay");
+
+
+    //if both string validate then run the function to get the average color
+    if (validateHex(color1) && validateHex(color2)) {
+        infoDev.innerHTML = "";
+        var meanColor = averageColor(color1, color2, true);
+        feedbackDiv.setAttribute
+            ("style", "background-color:" + meanColor + ";");
+        infoDev.innerHTML = meanColor;
+    }
+    else {
+        infoDev.innerHTML = "Please supply two valid color strings...";
+        feedbackDiv.setAttribute
+            ("style", "background-color: #ffffff;");
+    }
+
+
+    return false;
+}
+
+/*
+End UI input for testing of colory function
+*/
---
+++ JS/html.js
@@ -0,0 +1,113 @@
+﻿/*
+utility functions
+*/
+
+function elById(ID) {
+    return document.getElementById(ID);
+}
+
+function decodeHTML(str) {
+
+    str = str.replace(/&/g, "&amp;");
+    str = str.replace(/>/g, "&gt;");
+    str = str.replace(/</g, "&lt;");
+    str = str.replace(/"/g, "&quot;");
+
+    return str;
+
+}
+/*
+End utility functions
+*/
+
+
+
+/*
+Main html functions
+*/
+function iterateHtmlArray(array) {
+
+    var str = "";
+    var strEnd = "";
+
+    array.forEach((item, index) => {
+
+        //if item is array recurse back throuhg the main function
+        if (Array.isArray(item)) {
+            str += iterateHtmlArray(item);
+        }
+        else {
+            //generate html based on items within the current array
+            if (array.length == 1) {
+                str += "<" + item + "/>";
+            }
+            else {
+                if (index == 0) {
+                    str = "<" + item + ">";
+                    strEnd += "</" + item + ">";
+                }
+                else {
+                    if (!item == "") {
+                        str += decodeHTML(item);
+                    }
+                }
+            }
+        }
+
+    });
+
+    //return html string
+    return str + strEnd;
+}
+/*
+End main html functions
+*/
+
+
+
+/*
+UI input for testing of html array function
+*/
+function submitHtmlFunction(id) {
+
+    var arr = [];
+    var outputString = "";
+
+    switch (id) {
+        case 1:
+        arr = ["div"];
+        outputString = iterateHtmlArray(arr);
+        break;
+        case 2:
+        arr = ["h1", "Text"];
+        outputString = iterateHtmlArray(arr);
+        break;
+        case 3:
+        arr = ["span", "More", "Text"];
+        outputString = iterateHtmlArray(arr);
+        break;
+        case 4:
+        arr = ["a", ["b", "More"], " Text"];
+        outputString = iterateHtmlArray(arr);
+        break;
+        case 5:
+        arr = ["p", "<b>Text</b>"];
+        outputString = iterateHtmlArray(arr);
+        break;
+        case 6:
+        arr = ["div", ["a", ["h1", "Even"], "More"], " Text", " And More"];
+        outputString = iterateHtmlArray(arr);
+        break;
+    }
+
+    var outputDiv = elById("ReturnedHtml");
+
+    outputDiv.innerText = outputString;
+
+}
+
+/*
+End UI functions
+*/
+
+
